3378c513b2f151416f8600d059dc1ea1e3a5af6b
nishita@KNOESIS.ORG <nishita@KNOESIS.ORG@4a20b28f-9ff2-4c75-87a8-8ae1daa1b4dd>
2014-11-13 16:22:44 +0000

    shell file

--- ResQu/resqu.sh
+++ ResQu/resqu.sh
@@ -6,6 +6,17 @@ echo "------------------------------------------"
 ant compile citation-driver
 echo "------------------------------------------"
 
+echo "------------------------------------------"
+echo "STEP2: SEMREP THE MEDLINECITATIONS..."
+echo "------------------------------------------"
+ant compile semrep-driver
+echo "------------------------------------------"
+
+echo "------------------------------------------"
+echo "STEP3: SUMMARIZING THE SEMREPED CITATIONS..."
+echo "------------------------------------------"
+./sum-script.sh
+echo "------------------------------------------"
 
 end=$(date +"%s")
 rawextime=`expr $end - $start`
