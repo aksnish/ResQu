590054225827d698b884f204a366a0ab3e9b1736
nishita@KNOESIS.ORG <nishita@KNOESIS.ORG@4a20b28f-9ff2-4c75-87a8-8ae1daa1b4dd>
2014-11-13 16:09:05 +0000

    new drivers

---
+++ src/drivers/SummarizeDiver.java
@@ -0,0 +1,51 @@
+package drivers;
+
+import java.io.File;
+import java.io.IOException;
+
+
+/*Script implemented in sum-script.sh */
+public class SummarizeDiver {
+
+
+	public static void main(String[] args) throws IOException {
+
+		Process pr = null;
+		Runtime rt = Runtime.getRuntime();
+		StringBuilder command = new StringBuilder();
+		String filename = null, preferred_name = null;
+		
+		String base_path= "/home/nishita/workspace/ResQu/sem/";
+		String repo ="/home/nishita/Documents/Projects/Summarization-2012-SPA/bin/";
+		
+		String[] cmd = { "/bin/bash", "cd /home/nishita/Documents/Projects/Summarization-2012-SPA/bin/", "ls -l"};
+		
+		File directory = new File("sem/");
+		File[] directoryListing = directory.listFiles();
+
+		if (directoryListing != null) {
+			for (File file : directoryListing) {
+
+				filename = file.toString().substring(file.toString().indexOf("/")+1,file.toString().length());
+				preferred_name = file.toString().substring(file.toString().indexOf("/")+1,file.toString().indexOf("_"));
+
+				System.out.println(filename + "\t" + preferred_name);
+				
+				pr = rt.exec(cmd);
+				System.out.println(pr.getOutputStream().toString());
+				
+				command = new StringBuilder();
+				command.append("perl summarize_test.pl " +base_path + filename +" "+ preferred_name);
+				pr = rt.exec(command.toString());
+				//System.out.println(command);
+
+				//go back to project directory
+				break;
+			} 
+		}
+		pr.destroy();
+	}
+}
+
+
+//perl summarize_test.pl Test.txt Asthma
---
+++ src/drivers/SummarizedPredicationExtractorDriver.java
@@ -0,0 +1,33 @@
+package drivers;
+
+import gov.nih.nlm.semrep.utils.SemrepTripleExtractor;
+
+import java.io.File;
+
+public class SummarizedPredicationExtractorDriver {
+
+
+	public static void main (String args [])
+	{
+		SemrepTripleExtractor semrep = new SemrepTripleExtractor();
+		String inputFile;
+		String outputFile;
+		File folder = new File("semrep/");
+		File[] listOfFiles = folder.listFiles();
+		for (File file : listOfFiles) {
+			if (accept(file.toString())) {
+				inputFile = file.toString();
+				outputFile = file.toString().substring(0, file.toString().indexOf(".semrep"));
+				semrep.getFilePredications(inputFile, outputFile);
+			}
+		}
+	}
+
+	public static boolean accept(final String name) {
+		if(name.endsWith("salient_predications"))
+			return true;
+		else
+			return false;
+	}
+
+}
