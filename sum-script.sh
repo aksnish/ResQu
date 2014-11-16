fc71f614d2f42d9d1eb02cc083a8bff4374479b2
nishita@KNOESIS.ORG <nishita@KNOESIS.ORG@4a20b28f-9ff2-4c75-87a8-8ae1daa1b4dd>
2014-11-16 01:31:13 +0000

    now points to Summarization-2012;

--- sum-script.sh
+++ sum-script.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-cd /home/nishita/Documents/Projects/Summarization2006-AR/bin/
-base_dir="/home/nishita/workspace/ResQu/semrep/"
+cd /home/nishita/Documents/Projects/Summarization-2012-SPA/bin/
+base_dir="/home/nishita/workspace/ResQu/data/semrep/"
 base_dir_files="$base_dir*"
 for file in $base_dir_files
 do
     if [[ $file == *semrep ]]
     then
 	IFS='/' read -a ary <<< "${file}"
-	filename=${ary[6]};
+	filename=${ary[7]};
 	IFS='_' read -a myary <<< "${filename}"
 	#preferred_name=${myary[0]}
 	preferred_name=${filename/_[A-Z].semrep/ }
 	myFile="$base_dir$filename"
 	perl summarize_treatment.pl $myFile $preferred_name
-	#echo "$filename $myFile $preferred_name"
+	echo "$filename $myFile $preferred_name"
 	break;
     fi	
 done
