2f2c0800e80a1e2f84f6eab60e213d552ca60c05
nishita@KNOESIS.ORG <nishita@KNOESIS.ORG@4a20b28f-9ff2-4c75-87a8-8ae1daa1b4dd>
2014-11-20 23:08:22 +0000

    adding code from khpco

--- src/gov/nih/nlm/neo4j/UniqueNodes.java
+++ src/gov/nih/nlm/neo4j/UniqueNodes.java
@@ -8,55 +8,77 @@ import java.util.Map;
 
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 //import com.personal.neo4j.Main.RelTypes;
 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+
+
 public class UniqueNodes {
 
 	public void createNodespace() throws IOException {
-		GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase( "db/graphDB" );
-
+		GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase( "data/neo2" );
 		try (Transaction tx = graphDb.beginTx()) {
 
-			FileReader fr = new FileReader("RelTypes");
+			FileReader fr = new FileReader("test.txt");
 			@SuppressWarnings("resource")
 			BufferedReader br = new BufferedReader(fr);
 			String line = "";
 
 			Map<String, Node> check = new HashMap<String, Node>();
 			while ((line = br.readLine()) != null) {
-
-				String s = line;
-				String[] sa = s.split("-");
-
+				if(line.contains("|")){
+					line = line.replaceAll("\\|\\|\\|", "\\|").replaceAll("\\|\\|", "");
 
 
-				if (!check.containsKey(sa[0])) {
-					Node node1 = graphDb.createNode();
-					node1.setProperty("id", sa[0]);
-					check.put(sa[0], node1);
+					String s = line;
+					String[] sa = s.split("\\|");
+
+					if (!check.containsKey(sa[2])) {
+						Node node1 = graphDb.createNode();
+						node1.setProperty("CUI", sa[2]);
+						node1.setProperty("name",sa[3]);
+						node1.setProperty("semantic_type",sa[4]);
+						sa[0]=sa[0].substring(0, sa[0].indexOf("."));
+						node1.setProperty("PMID",sa[0]);
+						check.put(sa[2], node1);
+					}
+
+					if (!check.containsKey(sa[7])) {
+						Node node2 = graphDb.createNode();
+						node2.setProperty("CUI", sa[7]);
+						node2.setProperty("name", sa[8]);
+						node2.setProperty("semantic_type",sa[9]);
+						check.put(sa[7], node2);
+					}
+
+					//check.get(sa[2]).createRelationshipTo(check.get(sa[7]),RelTypes.TREATS);
 				}
-				if (!check.containsKey(sa[1])) {
-					Node node2 = graphDb.createNode();
-					node2.setProperty("id", sa[1]);
-					check.put(sa[1], node2);
-				}
-
-				//check.get(sa[0]).createRelationshipTo(check.get(sa[1]), RelTypes.TREATS);
-
-
 			}
 
 			tx.success();
+			System.out.println("Done");
 		}
-	}
-
-	public static void main (String args []) throws IOException{
-
-		UniqueNodes un = new UniqueNodes();
-		un.createNodespace();
 
 	}
+
+//	public static void main (String args []) throws IOException{
+
+//		UniqueNodes un = new UniqueNodes();
+//		un.createNodespace();
+
+//	}
 }
