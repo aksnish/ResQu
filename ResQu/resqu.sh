ae1152f49e6f72b1e914965810666e38ce3c19c3
nishita@KNOESIS.ORG <nishita@KNOESIS.ORG@4a20b28f-9ff2-4c75-87a8-8ae1daa1b4dd>
2014-11-21 00:14:41 +0000

    add jericho task;

--- ResQu/resqu.sh
+++ ResQu/resqu.sh
@@ -3,27 +3,33 @@ start=$(date +"%s")
 echo "------------------------------------------"
 echo "STEP1: GETTING 500 CITATIONS FROM MEDLINE..."
 echo "------------------------------------------"
-#ant compile citation-driver
+ant compile citation-driver
 echo "------------------------------------------"
 
 echo "------------------------------------------"
 echo "STEP2: SEMREP THE MEDLINECITATIONS..."
 echo "------------------------------------------"
-#ant compile semrep-driver
+ant compile semrep-driver
 echo "------------------------------------------"
 
 echo "------------------------------------------"
 echo "STEP3: SUMMARIZING THE SEMREPED CITATIONS..."
 echo "------------------------------------------"
-#./sum-script.sh
+./sum-script.sh
 echo "------------------------------------------"
 
 
 
 echo "------------------------------------------"
-echo "STEP2: SEMREP THE MEDLINECITATIONS..."
+echo "STEP4: NEO4J STUFF..."
 echo "------------------------------------------"
 ant compile neo4j-driver
+echo "------------------------------------------"
+
+echo "------------------------------------------"
+echo "STEP5: JERICHO CRAWLING..."
+echo "------------------------------------------"
+ant compile jericho-driver
 echo "------------------------------------------"
 
 
