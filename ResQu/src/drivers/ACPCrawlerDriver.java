c3e009eb569d9ef5c62f9247957dcf1465f2f875
nishita@KNOESIS.ORG <nishita@KNOESIS.ORG@4a20b28f-9ff2-4c75-87a8-8ae1daa1b4dd>
2014-11-21 00:11:35 +0000

    jericho test driver;

--- ResQu/src/drivers/ACPCrawlerDriver.java
+++ ResQu/src/drivers/ACPCrawlerDriver.java
@@ -4,20 +4,21 @@ import java.util.Set;
 
 import net.htmlparser.jericho.HTMLElements;
 import gov.nih.nlm.crawler.ACPCrawler;
+import gov.nih.nlm.crawler.SimpleJericho;
 
 public class ACPCrawlerDriver {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
-		String startPage = "http://online.statref.com/document.aspx?docAddress=nTcI4TcIBL6Efuupqj9DJg%3d%3d&QueryID=-1&SessionID=1E6386AQWOTXIVHN#H&d430&ChaptersTab&nTcI4TcIBL6Efuupqj9DJg%3d%3d&&539";
+//		String startPage = "http://online.statref.com/document.aspx?docAddress=nTcI4TcIBL6Efuupqj9DJg%3d%3d&QueryID=-1&SessionID=1E6386AQWOTXIVHN#H&d430&ChaptersTab&nTcI4TcIBL6Efuupqj9DJg%3d%3d&&539";
+//		
+//		ACPCrawler crawler = new ACPCrawler();
+//		crawler.crawl(startPage);
+//		
+//		HTMLElements elements = null;
+//		Set<String> blockTags =  elements.getBlockLevelElementNames();
 		
-		ACPCrawler crawler = new ACPCrawler();
-		crawler.crawl(startPage);
-		
-		HTMLElements elements = null;
-		Set<String> blockTags =  elements.getBlockLevelElementNames();
-		
-	
-		
+			SimpleJericho sj = new SimpleJericho();
+			sj.crawlURL();
 	}
 }
