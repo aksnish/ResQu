94f381e6aeea4f5bc5d661747b6c9b71a3a606de
nishita@KNOESIS.ORG <nishita@KNOESIS.ORG@4a20b28f-9ff2-4c75-87a8-8ae1daa1b4dd>
2014-11-23 22:01:32 +0000

    new crawler code;

--- src/drivers/ACPCrawlerDriver.java
+++ src/drivers/ACPCrawlerDriver.java
@@ -1,24 +1,41 @@
 package drivers;
 
-import java.util.Set;
-
-import net.htmlparser.jericho.HTMLElements;
-import gov.nih.nlm.crawler.ACPCrawler;
-import gov.nih.nlm.crawler.SimpleJericho;
+import java.util.ArrayList;
+import java.util.List;
+import net.htmlparser.jericho.Tag;
+import gov.nih.nlm.crawler.ClinicalResourceCrawler;
 
 public class ACPCrawlerDriver {
 
 	public static void main(String[] args) throws Exception {
+		List<Tag> fontTagList = new ArrayList<Tag>();
+		ClinicalResourceCrawler crawl = new ClinicalResourceCrawler();
+		String startPage ="file:jericho/Acetominophen.html";
+		if (startPage.indexOf(':')==-1) startPage="file:"+startPage;
+
+		//crawl.getBoldTextIndex(startPage);
+
+				System.out.println("Source Start page : "+startPage);
+				int startIndex= crawl.getBoldTextIndex(startPage,"Uses");
+				int endIndex= crawl.getBoldTextIndex(startPage,"Dosage and Administration");
+				System.out.println(startIndex +" "+endIndex);
+				fontTagList = crawl.findFontBetween(startPage, startIndex,endIndex);
 		
-//		String startPage = "http://online.statref.com/document.aspx?docAddress=nTcI4TcIBL6Efuupqj9DJg%3d%3d&QueryID=-1&SessionID=1E6386AQWOTXIVHN#H&d430&ChaptersTab&nTcI4TcIBL6Efuupqj9DJg%3d%3d&&539";
-//		
-//		ACPCrawler crawler = new ACPCrawler();
-//		crawler.crawl(startPage);
-//		
-//		HTMLElements elements = null;
-//		Set<String> blockTags =  elements.getBlockLevelElementNames();
+				for(Tag font : fontTagList){
+					//System.out.println("----------------");
+					//System.out.println(font.getElement());
+					System.out.println("USES : "+font.getElement().getTextExtractor());
+				}
 		
-			SimpleJericho sj = new SimpleJericho();
-			sj.crawlURL();
+				startIndex= crawl.getBoldTextIndex(startPage,"Cautions");
+				endIndex= crawl.getBoldTextIndex(startPage,"Interactions");
+		
+				fontTagList = crawl.findFontBetween(startPage, startIndex,endIndex);
+				System.out.println("----------------");
+				for(Tag font : fontTagList){
+		
+					//System.out.println(font.getElement());
+					System.out.println("CAUTIONS : "+font.getElement().getTextExtractor());
+				}
 	}
 }
