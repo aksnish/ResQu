87f0d790c1656841e3d3910dcdc69bb2cb5cfcef
nishita@KNOESIS.ORG <nishita@KNOESIS.ORG@4a20b28f-9ff2-4c75-87a8-8ae1daa1b4dd>
2014-11-23 23:36:21 +0000

    write metamap files;

--- src/drivers/ACPCrawlerDriver.java
+++ src/drivers/ACPCrawlerDriver.java
@@ -1,41 +1,74 @@
 package drivers;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+
 import net.htmlparser.jericho.Tag;
 import gov.nih.nlm.crawler.ClinicalResourceCrawler;
+import gov.nih.nlm.utils.Constants;
 
 public class ACPCrawlerDriver {
 
+	static PrintStream OUT;
+
+
 	public static void main(String[] args) throws Exception {
-		List<Tag> fontTagList = new ArrayList<Tag>();
-		ClinicalResourceCrawler crawl = new ClinicalResourceCrawler();
-		String startPage ="file:jericho/Acetominophen.html";
-		if (startPage.indexOf(':')==-1) startPage="file:"+startPage;
 
-		//crawl.getBoldTextIndex(startPage);
+		//OUT = System.out;
 
-				System.out.println("Source Start page : "+startPage);
+		File folder = new File("data/jericho/");
+		File[] listOfFiles = folder.listFiles();
+
+		for (File file : listOfFiles) {
+			if (file.isFile()) {
+				//System.out.println(file.getName());
+				String filename = Constants.METAMAP_FOLDER_PATH+file.getName();
+
+				OUT = new PrintStream(new File(filename));
+				List<Tag> fontTagList = new ArrayList<Tag>();
+				ClinicalResourceCrawler crawl = new ClinicalResourceCrawler();
+				//String startPage ="file:"+file.getName();//jericho/Acetominophen.html";
+
+				String startPage =Constants.CRAWLER_FILE_PREFIX+Constants.CRAWLER_DATA_FOLDER_PATH+file.getName();
+
+				if (startPage.indexOf(':')==-1) startPage="file:"+startPage;
+
 				int startIndex= crawl.getBoldTextIndex(startPage,"Uses");
 				int endIndex= crawl.getBoldTextIndex(startPage,"Dosage and Administration");
-				System.out.println(startIndex +" "+endIndex);
+
+				String abs_elements = null;
+
 				fontTagList = crawl.findFontBetween(startPage, startIndex,endIndex);
-		
+				OUT.append("\nPMID  - " +1);
+				OUT.append("\nTI  - " +"title");
+				OUT.append("\nAB  - " );
 				for(Tag font : fontTagList){
-					//System.out.println("----------------");
-					//System.out.println(font.getElement());
-					System.out.println("USES : "+font.getElement().getTextExtractor());
+					if(("•").equalsIgnoreCase(abs_elements))
+						continue;
+					abs_elements = font.getElement().getTextExtractor().toString();
+					OUT.append(abs_elements.replaceAll("[^\\x00-\\x7F]", "")+"\n");
 				}
-		
+
 				startIndex= crawl.getBoldTextIndex(startPage,"Cautions");
 				endIndex= crawl.getBoldTextIndex(startPage,"Interactions");
-		
 				fontTagList = crawl.findFontBetween(startPage, startIndex,endIndex);
-				System.out.println("----------------");
+
+				OUT = new PrintStream(new File(filename.replaceAll("html", "txt")));
+				OUT.append("\nPMID  - " +1);
+				OUT.append("\nTI  - " +"title");
+				OUT.append("\nAB  - " );
+
 				for(Tag font : fontTagList){
-		
-					//System.out.println(font.getElement());
-					System.out.println("CAUTIONS : "+font.getElement().getTextExtractor());
+					abs_elements = font.getElement().getTextExtractor().toString();
+					if(("•").equalsIgnoreCase(abs_elements))
+						continue;
+					OUT.append(abs_elements.replaceAll("[^\\x00-\\x7F]", "")+"\n");
 				}
+			}
+		}
 	}
 }
