b82d4eb3e59e1c061ef3cb5234f2a10db81ebf85
nishita@KNOESIS.ORG <nishita@KNOESIS.ORG@4a20b28f-9ff2-4c75-87a8-8ae1daa1b4dd>
2014-11-27 22:42:10 +0000

    FileGenerator now creates directories dynamically;

--- ResQu/src/gov/nih/nlm/utils/Constants.java
+++ ResQu/src/gov/nih/nlm/utils/Constants.java
@@ -19,10 +19,11 @@ public class Constants {
 	public static final String METAMAP_COMMAND = "metamap -J -I -Y dsyn,fndg,inpo,phsu";
 
 	public static final String QUERY = "";
-	public static final String DATA_FOLDER = "/usr/share/data/resqu/data/";
+	public static final String DATA_FOLDER = "data/";//"/usr/share/data/resqu/data/";
+	public static final String INPUT_FOLDER ="input/";
 	public static final String DISEASE_LIST=DATA_FOLDER+"disease_topic_list.txt";
-	public static final String TOPIC_DISEASE_FOLDER = DATA_FOLDER+"disease/";
-	public static final String TOPIC_DRUG_FILE = DATA_FOLDER+"drug/";
+	public static final String DISEASE_FOLDER = DATA_FOLDER+INPUT_FOLDER+"disease/";
+	public static final String DRUG_FOLDER = DATA_FOLDER+INPUT_FOLDER+"drug/";
 	public static final String SEMREP_FOLDER = DATA_FOLDER+"semrep/";
 	
 	public static final String SUMMARIZE_FOLDER = DATA_FOLDER+"summarized-triples/";
@@ -33,10 +34,8 @@ public class Constants {
 	public static final String CRAWLER_DATA_FOLDER="jericho/";
 	public static final String CRAWLER_DATA_FOLDER_PATH =DATA_FOLDER+CRAWLER_DATA_FOLDER;
 
-	public static final String METAMAP_INPUT_FOLDER = "metamap-input/";
-	public static final String METAMAP_INPUT_FOLDER_PATH =DATA_FOLDER+METAMAP_INPUT_FOLDER;
-	public static final String METAMAP_OUTPUT_FOLDER = "metamap-output/";
-	public static final String METAMAP_OUTPUT_FOLDER_PATH =DATA_FOLDER+METAMAP_OUTPUT_FOLDER;
+	public static final String METAMAP_INPUT_FOLDER =DATA_FOLDER+"metamap-input/";
+	public static final String METAMAP_OUTPUT_FOLDER =DATA_FOLDER+"metamap-output/";
 	
 	public static final String SERIALZED_MAPS_FOLDER = DATA_FOLDER+"serializedMaps/";
 	public static final String SUM_DOC_DIR = DATA_FOLDER+"summary-docs/";
--- ResQu/src/gov/nih/nlm/utils/FilenameGenerator.java
+++ ResQu/src/gov/nih/nlm/utils/FilenameGenerator.java
@@ -1,5 +1,8 @@
 package gov.nih.nlm.utils;
 
+import java.io.File;
+import java.io.IOException;
+
 public class FilenameGenerator {
 
 	public static final char FILE_SEPERATOR = '/';
@@ -12,7 +15,7 @@ public class FilenameGenerator {
 		int lastSepPos = filename.lastIndexOf(FILE_SEPERATOR);
 		return lastSepPos;
 	}
-	
+
 	public  String normalizeFileName(String filename){
 		if(filename == null){
 			return null;
@@ -22,22 +25,65 @@ public class FilenameGenerator {
 		String newName = filename.substring(bpos, epos).replaceAll(".txt", "");
 		return newName;
 	}
-	
+
+	@Deprecated
 	public String setFilename (String querystr, String topic){
 		String filename;
 		if(querystr.contains(Constants.DRUG_THERAPY))
 			filename = topic+Constants.COMPLEX_EXTENSION;
 		else 
 			filename = topic+Constants.SIMPLE_EXTENSION;
-		
+
 		filename=filename.replaceAll(" ", "_");
 		return filename;
 	}
-	
-	public static void main(String[] args) {
-		FilenameGenerator fg = new FilenameGenerator();
-		String file = fg.normalizeFileName("data/disease/Migraine Disorders_S.txt");
-		System.out.println(file);
+
+	public String setFilename (String directoryPrefix, String querystr, String topic, int i) throws IOException{
+		String filename = null;
+		String format , result = null;
+
+		if(i<10){
+			format = String.format("%%0%dd", 2);
+			result = String.format(format, i);
+		}
+		else{
+			result = Integer.toString(i);
+		}
+		
+		//Constants.DISEASE_FOLDER
+		String dir = directoryPrefix+result+"-"+topic;
+		dir=dir.replaceAll(" ", "_");
+		File directory = new File(dir);
+
+		if (!directory.exists()) {
+			directory.mkdirs();
+
+			System.out.println("Dir : "+directory);
+			if(querystr.contains(Constants.DRUG_THERAPY)){
+				filename = directory+"/"+topic+Constants.COMPLEX_EXTENSION;
+			}else{ 
+				filename = directory+"/"+topic+Constants.SIMPLE_EXTENSION;
+			}
+			filename=filename.replaceAll(" ", "_");
+		}
+		else{
+			System.out.println("directory already exists"+directory);
+			if(querystr.contains(Constants.DRUG_THERAPY))
+			{
+				filename = directory+"/"+topic+Constants.COMPLEX_EXTENSION;
+			}else{ 
+				filename = directory+"/"+topic+Constants.SIMPLE_EXTENSION;
+			}
+			filename=filename.replaceAll(" ", "_");
+		}
+		return filename;
 	}
+
+
+	//	public static void main(String[] args) {
+	//		FilenameGenerator fg = new FilenameGenerator();
+	//		String file = fg.normalizeFileName("data/disease/Migraine Disorders_S.txt");
+	//		System.out.println(file);
+	//	}
 
 }
