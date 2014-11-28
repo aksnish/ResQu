3790c1d56fe24860935f834550f3afe9a3a1e09e
nishita@KNOESIS.ORG <nishita@KNOESIS.ORG@4a20b28f-9ff2-4c75-87a8-8ae1daa1b4dd>
2014-11-28 23:05:37 +0000

    done;

--- ResQu/src/drivers/LuceneIndexReaderDriver.java
+++ ResQu/src/drivers/LuceneIndexReaderDriver.java
@@ -1,14 +1,22 @@
 package drivers;
 
 import java.io.IOException;
+import java.util.List;
+
+import org.apache.lucene.document.Document;
 
 import gov.nih.nlm.core.lucene.LuceneIndexReader;
+import gov.nih.nlm.core.similarity.measures.CosineSimilarity;
+import gov.nih.nlm.model.DocVector;
 
 public class LuceneIndexReaderDriver {
 	public static void main(String[] args) throws IOException {
-		String lucene_index_dir = "data/input_lucene";
+		String lucene_index_dir = "data/input_lucene/";
 		LuceneIndexReader indexReader = new LuceneIndexReader(lucene_index_dir);
-		indexReader.readDocs();
-		System.out.println("Total number of documents : "+indexReader.maxDocs());
+		CosineSimilarity cosine = new CosineSimilarity();
+		List<Document> docList =indexReader.getDocs();
+		List<DocVector> docVectrorList = indexReader.getDocVectors(docList); 
+		double cosineSim = cosine.getCosineSimilarity(docVectrorList);
+		indexReader.close();
 	}
 }
