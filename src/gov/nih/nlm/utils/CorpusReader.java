8a0903224acf24d691d4781ae4f9b45ded2dbfa7
nishita@KNOESIS.ORG <nishita@KNOESIS.ORG@4a20b28f-9ff2-4c75-87a8-8ae1daa1b4dd>
2014-11-28 19:22:08 +0000

    stub complete;

--- src/gov/nih/nlm/utils/CorpusReader.java
+++ src/gov/nih/nlm/utils/CorpusReader.java
@@ -1,22 +1,36 @@
 package gov.nih.nlm.utils;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CorpusReader {
+	String corpus_dir;
 
 	public CorpusReader(String input_dir) {
-		// TODO Auto-generated constructor stub
+		this.corpus_dir = input_dir;
 	}
 
 	public List<String> getFiles() {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.println("Getting the list of files from : " + corpus_dir);
+		DirectoryFileListIterator dirfile = new DirectoryFileListIterator();
+		List<String> fileList = new ArrayList<String>();
+		fileList = dirfile.getDirectoryFileList(corpus_dir);
+		return fileList;
 	}
 
-	public String getContent(String file_path) {
-		return file_path;
-		// TODO Auto-generated method stub
-		
+	public String getContent(String file_path) throws IOException {
+		System.out.println("Getting the content");
+		BufferedReader bufReader = null;
+		StringBuffer buf = new StringBuffer(" ");
+		bufReader = new BufferedReader(new FileReader(file_path));
+		while(bufReader.readLine()!=null){
+			buf.append(bufReader.readLine());
+			buf.append("\n");
+		}
+		bufReader.close();
+		return buf.toString();
 	}
-
 }
