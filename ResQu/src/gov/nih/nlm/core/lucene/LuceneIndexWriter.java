73b4410639ae413b0661d9b34ac7cfb6d94b2bb0
nishita@KNOESIS.ORG <nishita@KNOESIS.ORG@4a20b28f-9ff2-4c75-87a8-8ae1daa1b4dd>
2014-11-28 19:23:04 +0000

    stub complete;

--- ResQu/src/gov/nih/nlm/core/lucene/LuceneIndexWriter.java
+++ ResQu/src/gov/nih/nlm/core/lucene/LuceneIndexWriter.java
@@ -2,9 +2,11 @@ package gov.nih.nlm.core.lucene;
 
 import java.io.File;
 import java.io.IOException;
-
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.FieldType;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
@@ -16,14 +18,15 @@ public class LuceneIndexWriter {
 
 	private IndexWriter indexWriter = null;
 	private Analyzer analyzer = null;
+	private int id = 0;
+	private Directory directory;
 
 	public LuceneIndexWriter(String dir){
 		try {
 			analyzer = new StandardAnalyzer();
 			if (indexWriter == null) {
 				IndexWriterConfig indexConfig = new IndexWriterConfig(Version.LUCENE_4_10_2, analyzer);
-
-				Directory directory = FSDirectory.open(new File(dir));
+				directory = FSDirectory.open(new File(dir));
 				indexWriter = new IndexWriter(directory, indexConfig);
 			}
 		}catch (IOException e) {
@@ -31,16 +34,28 @@ public class LuceneIndexWriter {
 		}
 	}
 
-	public void write (String content){
-		//		  Document doc = new Document();
-		//		    String text = "This is the text to be indexed.";
-		//		    doc.add(new Field("fieldname", text, TextField.TYPE_STORED));
-		//		    iwriter.addDocument(doc)
+	public void write(String content){
+		System.out.println("------------------------------------");
+		System.out.println("Writing to directory : " + directory);
+		try {
+			FieldType fieldType = new FieldType();
+			fieldType.setStored(true);
+			fieldType.setIndexed(true);
+			fieldType.setStoreTermVectors(true);
+			Document doc = new Document();
+			doc.add(new Field("content", content, fieldType)); 
+			doc.add(new Field("docId", (id++) +"", fieldType));
+			indexWriter.addDocument(doc);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void close(){
 		try {
+			indexWriter.commit();
 			indexWriter.close();
+			directory.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
