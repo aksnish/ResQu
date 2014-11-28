dafcdab9032e215246859470373217c92ab83ced
nishita@KNOESIS.ORG <nishita@KNOESIS.ORG@4a20b28f-9ff2-4c75-87a8-8ae1daa1b4dd>
2014-11-28 23:06:01 +0000

    modified Object;

--- src/gov/nih/nlm/model/DocVector.java
+++ src/gov/nih/nlm/model/DocVector.java
@@ -1,24 +1,28 @@
 package gov.nih.nlm.model;
 
 import java.util.Map;
+
 import org.apache.commons.math3.linear.OpenMapRealVector;
 import org.apache.commons.math3.linear.RealVectorFormat;
 import org.apache.commons.math3.linear.SparseRealVector;
 
 public class DocVector {
 
-	public Map<String,Integer> terms;
+	public Map<String,Integer> dictionary;
 	public SparseRealVector vector;
+	public int id;
+	public String content;
 
-	public DocVector(Map<String,Integer> terms) {
-		this.terms = terms;
-		this.vector = new OpenMapRealVector(terms.size());
+	public DocVector(int id, Map<String,Integer> dictionary) {
+		this.id = id;
+		this.dictionary = dictionary;
+		this.vector = new OpenMapRealVector(dictionary.size());
 	}
 
-	public void setEntry(String term, int freq) {
-		if (terms.containsKey(term)) {
-			int pos = terms.get(term);
-			vector.setEntry(pos, (double) freq);
+	public void setEntry(String term, double freq) {
+		if (dictionary.containsKey(term)) {
+			int pos = dictionary.get(term);
+			vector.setEntry(pos, freq);
 		}
 	}
 
@@ -31,6 +35,12 @@ public class DocVector {
 		RealVectorFormat formatter = new RealVectorFormat();
 		return formatter.format(vector);
 	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+
 }
 
 
