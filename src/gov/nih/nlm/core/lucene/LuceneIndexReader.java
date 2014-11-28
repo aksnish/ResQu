7abb5440456196367d963a5fc8558af78b0c7e15
nishita@KNOESIS.ORG <nishita@KNOESIS.ORG@4a20b28f-9ff2-4c75-87a8-8ae1daa1b4dd>
2014-11-28 23:05:04 +0000

    done;

--- src/gov/nih/nlm/core/lucene/LuceneIndexReader.java
+++ src/gov/nih/nlm/core/lucene/LuceneIndexReader.java
@@ -1,42 +1,111 @@
 package gov.nih.nlm.core.lucene;
 
+import gov.nih.nlm.model.DocVector;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.DocsEnum;
 import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.SlowCompositeReaderWrapper;
+import org.apache.lucene.index.Terms;
+import org.apache.lucene.index.TermsEnum;
+import org.apache.lucene.search.DocIdSetIterator;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.BytesRef;
 
 public class LuceneIndexReader {
 
 	private IndexReader indexReader = null;
 	private Directory directory ;
+	private Map<String,Integer> dictionary;
 
 	public LuceneIndexReader(String dir){
 		try {
 			if(indexReader==null){
 				directory = FSDirectory.open(new File(dir));
 				indexReader = DirectoryReader.open(directory);
+				createDictionary();
 			}
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
 
-	public void readDocs() throws IOException {
-		int numOfDocs = indexReader.numDocs();
-		System.out.println("number of documents : "+numOfDocs);
-		for ( int i = 0; i < numOfDocs; i++)
-		{
-			Document document = indexReader.document(i);
-			System.out.println("\n Document : " + document);
+	public List<Document> getDocs() {
+		List<Document> docList = new ArrayList<Document>();
+		Document doc;
+		try {
+			int numOfDocs = indexReader.maxDoc();
+			for ( int i = 0; i < numOfDocs; i++)
+			{
+				doc = indexReader.document(i);
+				docList.add(doc);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		indexReader.close();
+		return docList;
 	}
 
-	public int maxDocs (){
-		return indexReader.maxDoc();
+	public List<DocVector> getDocVectors(List<Document> docList) {
+		List<DocVector> docVecList = new ArrayList<DocVector>();
+		Document doc;
+		DocVector docVec;
+		String content;
+		try {
+			int numOfDocs = indexReader.maxDoc();
+			for ( int i = 0; i < numOfDocs; i++)
+			{
+				doc = indexReader.document(i);
+				int id = Integer.parseInt(doc.get("docId"));
+				content = doc.get("content");
+				Terms terms = indexReader.getTermVector(id, "content"); 
+				if (terms != null && terms.size() > 0) {
+					docVec = new DocVector(id, dictionary);
+					docVec.setContent(content);
+					TermsEnum termsEnum = terms.iterator(null); // access the terms for this field
+					BytesRef term = null;
+					while ((term = termsEnum.next()) != null) {// explore the terms for this field
+
+						docVec.setEntry(term.utf8ToString(), Double.parseDouble(Long.toString(termsEnum.totalTermFreq())));
+					}
+					docVecList.add(docVec);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return docVecList;
+	}
+
+	public void createDictionary(){
+		dictionary = new HashMap<String,Integer>();
+		try {
+			int pos = 0;
+			Terms indexTerms = SlowCompositeReaderWrapper.wrap(indexReader).terms("content");
+			TermsEnum termsEnum = indexTerms.iterator(null);
+			while ((termsEnum.next()) != null) {
+				dictionary.put(termsEnum.term().utf8ToString(), pos++);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+	}
+
+	public void close(){
+		try {
+			indexReader.close();
+			directory.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
