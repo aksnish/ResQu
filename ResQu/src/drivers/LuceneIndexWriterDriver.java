dc3814ac0881ebe5814fe7abfce0da7fd20d2a9e
nishita@KNOESIS.ORG <nishita@KNOESIS.ORG@4a20b28f-9ff2-4c75-87a8-8ae1daa1b4dd>
2014-11-28 19:22:52 +0000

    writer stub complete;

--- ResQu/src/drivers/LuceneIndexWriterDriver.java
+++ ResQu/src/drivers/LuceneIndexWriterDriver.java
@@ -1,21 +1,25 @@
 package drivers;
 
+import java.io.IOException;
 import java.util.List;
 
 import gov.nih.nlm.core.lucene.LuceneIndexWriter;
+import gov.nih.nlm.utils.Constants;
 import gov.nih.nlm.utils.CorpusReader;
 
 public class LuceneIndexWriterDriver {
 
-	public static void main(String[] args) {
-		String input_dir ="data/input_lucene/";
-		CorpusReader reader = new CorpusReader(input_dir); 
-		String dir = null;
-		LuceneIndexWriter writer = new LuceneIndexWriter(dir);
+	public static void main(String[] args) throws IOException {
+		String lucene_index_dir ="data/input_lucene/";
+		String corpus_dir = Constants.SUM_DOC_DIR;
+		CorpusReader reader = new CorpusReader(corpus_dir);
 		List<String> files = reader.getFiles();
 		String content;
+
+		LuceneIndexWriter writer = new LuceneIndexWriter(lucene_index_dir);
 		for(String file_path: files){
 			content = reader.getContent(file_path);
+			//System.out.println(content);
 			writer.write(content);
 		}
 		writer.close();
