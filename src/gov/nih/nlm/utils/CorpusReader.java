255e3ad19450df983ba7af84210ec15f58818068
nishita@KNOESIS.ORG <nishita@KNOESIS.ORG@4a20b28f-9ff2-4c75-87a8-8ae1daa1b4dd>
2014-11-28 18:31:59 +0000

    Corpus reader class;

---
+++ src/gov/nih/nlm/utils/CorpusReader.java
@@ -0,0 +1,22 @@
+package gov.nih.nlm.utils;
+
+import java.util.List;
+
+public class CorpusReader {
+
+	public CorpusReader(String input_dir) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<String> getFiles() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getContent(String file_path) {
+		return file_path;
+		// TODO Auto-generated method stub
+		
+	}
+
+}
